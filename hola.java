09a2ba965306ff257eb530d33ac72ee102a01acf
ACG <test>
2017-12-01 23:09:09 -0300

    Inicio. version 0.1

---
+++ hola.java
@@ -0,0 +1,5 @@
+total 4
+drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:39 ./
+drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:29 ../
+drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:33 .git/
+-rw-r--r-- 1 Andres Cruz 197121 0 dic  1 22:43 hola.java
