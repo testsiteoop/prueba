0f0d978e8ec468a3e6a698028392b7e07c701e19
ACG <test>
2017-12-01 23:14:32 -0300

    v2

--- hola.java
+++ hola.java
@@ -3,3 +3,8 @@ drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:39 ./
 drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:29 ../
 drwxr-xr-x 1 Andres Cruz 197121 0 dic  1 22:33 .git/
 -rw-r--r-- 1 Andres Cruz 197121 0 dic  1 22:43 hola.java
+total 3
+-rw-r--r-- 1 Andres Cruz 197121  20 dic  1 22:56 archivoIgnorado.hola
+-rw-r--r-- 1 Andres Cruz 197121 219 dic  1 22:43 hola.java
+-rw-r--r-- 1 Andres Cruz 197121   0 dic  1 22:57 hola2.java
+-rw-r--r-- 1 Andres Cruz 197121 124 dic  1 22:46 log.log
---
+++ hola2.java
@@ -0,0 +1,5 @@
+total 3
+-rw-r--r-- 1 Andres Cruz 197121  20 dic  1 22:56 archivoIgnorado.hola
+-rw-r--r-- 1 Andres Cruz 197121 473 dic  1 23:11 hola.java
+-rw-r--r-- 1 Andres Cruz 197121   0 dic  1 22:57 hola2.java
+-rw-r--r-- 1 Andres Cruz 197121 124 dic  1 22:46 log.log
